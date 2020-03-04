5996167761f96f940342db41712d20cdaddfbd37
rohit-tn <rohit@truenorth.co.in>
2020-03-04 16:27:16 +0530

    wrapped in Ul-Li tags

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,16 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   posts :any[] =  [{
+    body: "Meeting heading. <a href='https://meetinglink.truenorth.co.in'>Meeting Title</a>",
+  },{
     body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. "
   },{
     body: "Loremsa. Cum sociis natoque penatibus et magnis di"
   },{
-    body: "Meeting heading Lorem  elit. ",
-    img: "https://scontent.fcnq1-1.fna.fbcdn.net/v/t1.0-9/22491481_680661882130471_2500422036645997028_n.jpg?oh=2e5001e92d201e446e3120204869a434&oe=5A68A19D"
+    body: "Lorem ipsum dolor sit amet, consectetuer https://app.zeplin.io. "
   }];
 
   parseBody(body:string) {
-    // you will need to improve this regex.
+    // you will need to improve this regex. incase up want to convert a hyper link in text 
     return body.replace(/(http.*?\s)/, "<a href=\"$1\">$1</a>")
   }
 }
